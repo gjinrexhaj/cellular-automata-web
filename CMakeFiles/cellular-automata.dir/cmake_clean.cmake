c95642ecbb0a19b429ad4735df9da1321310ff50
gjin <74433905+gjinrexhaj@users.noreply.github.com>
2025-10-02 22:23:39 -0400

    Add files via upload

---
+++ CMakeFiles/cellular-automata.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cellular-automata.dir/src/cellstate_buffer.cpp.o"
+  "CMakeFiles/cellular-automata.dir/src/cellstate_buffer.cpp.o.d"
+  "CMakeFiles/cellular-automata.dir/src/grid.cpp.o"
+  "CMakeFiles/cellular-automata.dir/src/grid.cpp.o.d"
+  "CMakeFiles/cellular-automata.dir/src/main.cpp.o"
+  "CMakeFiles/cellular-automata.dir/src/main.cpp.o.d"
+  "CMakeFiles/cellular-automata.dir/src/simulation.cpp.o"
+  "CMakeFiles/cellular-automata.dir/src/simulation.cpp.o.d"
+  "cellular-automata.html"
+  "cellular-automata.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cellular-automata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
